be38776db217a067c72146d633a8f5cb445382d3
Joris Falter <joris@jorisfalter.com>
2023-02-23 08:59:03 +0700

    x

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,41 @@
 import React from "react";
 React.useLayoutEffect = React.useEffect;
 import { useEffect } from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
-import 'react-perfect-scrollbar/dist/css/styles.css';
+import "react-perfect-scrollbar/dist/css/styles.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../public/assets/css/style.css";
 import "../public/assets/css/widgets.css";
 import "../public/assets/css/responsive.css";
-import 'metismenujs/dist/metismenujs.css';
+// import 'metismenujs/dist/metismenujs.css';
 
 function MyApp({ Component, pageProps }) {
-    const router = useRouter()
+  const router = useRouter();
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            window.WOW = require("wowjs");
-        }
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.WOW = require("wowjs");
+    }
 
-        new WOW.WOW().init();
+    new WOW.WOW().init();
 
-        const hasGridClass = document.querySelector('.grid-sizer');
-        
-        if (hasGridClass != null) {
-            const xyz = require("masonry-layout");
-            var msnry = new xyz(".grid", {
-                itemSelector: ".grid-item",
-                columnWidth: ".grid-sizer"
-            });
-        }    
-        
-        return () => {
-            router.events.off('routeChangeError', handleRouteChangeError)
-          }
-    }, []);
-    return <Component {...pageProps} />;
-    
+    const hasGridClass = document.querySelector(".grid-sizer");
+
+    if (hasGridClass != null) {
+      const xyz = require("masonry-layout");
+      var msnry = new xyz(".grid", {
+        itemSelector: ".grid-item",
+        columnWidth: ".grid-sizer",
+      });
+    }
+
+    return () => {
+      router.events.off("routeChangeError", handleRouteChangeError);
+    };
+  }, []);
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
