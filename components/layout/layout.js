b4e531277a141666f4ea300634cdd3e874f58ae1
Joris Falter <joris@jorisfalter.com>
2023-03-22 11:40:17 +0700

    x

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -8,36 +8,39 @@ import BackToTop from "../elements/backToTop";
 import Head from "next/head";
 
 const Layout = ({ children }) => {
-    const addClass = () => {
-        document.body.classList.add("canvas-opened");
-    };
+  const addClass = () => {
+    document.body.classList.add("canvas-opened");
+  };
 
-    const removeClass = () => {
-        document.body.classList.remove("canvas-opened");
-    };
+  const removeClass = () => {
+    document.body.classList.remove("canvas-opened");
+  };
 
-    const openSearch = () => {
-        document.body.classList.toggle("open-search-form");
-    };
+  const openSearch = () => {
+    document.body.classList.toggle("open-search-form");
+  };
 
-    return (
-        <>
-            <Head>
-                <title>Stories - Personal Blog HTML Template</title>
-                <meta property="og:title" content="My page title" key="title" />
-                <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,700;1,400&family=Noto+Sans+JP:wght@400;500;700;900&display=swap" rel="stylesheet" />
-            </Head>
-            <Sidebar removeClass={removeClass} />
-            <Header addClass={addClass} openSearch={openSearch} />
-            <Search />
+  return (
+    <>
+      <Head>
+        <title>Reggio Home</title>
+        <meta property="og:title" content="My page title" key="title" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,700;1,400&family=Noto+Sans+JP:wght@400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Sidebar removeClass={removeClass} />
+      <Header addClass={addClass} openSearch={openSearch} />
+      <Search />
 
-            {children}
+      {children}
 
-            <Bottom />
-            <Footer removeClass={removeClass} />
-            <BackToTop />
-        </>
-    );
+      <Bottom />
+      <Footer removeClass={removeClass} />
+      <BackToTop />
+    </>
+  );
 };
 
 export default Layout;
