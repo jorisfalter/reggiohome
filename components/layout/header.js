db7b7df0c1d862333aa59377cd15dc40b04a1af3
Joris Falter <joris@jorisfalter.com>
2023-03-28 13:34:51 +0700

    x

--- components/layout/header.js
+++ components/layout/header.js
@@ -7,7 +7,7 @@ const Header = ({ addClass, openSearch }) => {
       <header className="main-header header-style-1 font-heading">
         <div className="header-top">
           <div className="container">
-            <div className="row pt-20 pb-20">
+            <div className="row pt-10 pb-10">
               <div className="col-md-3 col-xs-6">
                 <Link href="/">
                   <a>
