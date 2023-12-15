82cbe432f78d77dc05acda3249235dae13a2559b
Joris Falter <joris@jorisfalter.com>
2023-12-15 09:49:17 +0700

    x

--- pages/index.js
+++ pages/index.js
@@ -82,7 +82,7 @@ function Home() {
                     width="100%"
                     height="320px"
                     style={{ border: "1px solid #EEE", background: "white" }}
-                    frameborder="0"
+                    frameBorder="0"
                     scrolling="no"
                   ></iframe>
                 </div>
